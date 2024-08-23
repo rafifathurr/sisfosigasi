de6064fe766d9c8aebfbf6e0152baaa021f69165
rafifathur <rafifathur.rahman20@gmai.com>
2024-08-23 20:45:51 +0700

    Feat : adding master migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,21 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->engine = 'InnoDB';
+            $table->integer('id')->autoIncrement();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
+            $table->char('phone', 13);
+            $table->text('address')->nullable();
+            $table->string('province_slug')->nullable();
+            $table->string('city_slug')->nullable();
+            $table->integer('parent_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+
+            // Foreign Key
+            $table->foreign('parent_id')->references('id')->on('users');
         });
     }
 
